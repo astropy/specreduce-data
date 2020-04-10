8d0909c1784a1da436a5e61dfc05daf090d1ec94
T. E. Pickering <te.pickering@gmail.com>
2020-04-09 18:40:40 -0700

    add basic ascii reader that tests will use

---
+++ specreduce-data/readers.py
@@ -0,0 +1,11 @@
+# utility functions for reading data files
+
+from astropy.table import Table
+
+
+def read_basic_ascii_table(table_file):
+    """
+    Try to read a file using AstroPy Table's basic ascii reader
+    """
+    t = Table.read(table_file, format='ascii'
+    return t
