7f959e2b708c1ef1dbc1d71d0cb6aadf1f3b3600
T. E. Pickering <te.pickering@gmail.com>
2020-04-09 18:40:57 -0700

    add initial test to read extinction files

---
+++ specreduce-data/tests/test_read_extinction.py
@@ -0,0 +1,18 @@
+# test to make sure extinction data is properly packaged and can be read in correctly
+
+import os
+import pkg_resources
+
+from ..readers import read_basic_ascii_table
+
+def test_read_extinction():
+    ext_dir = os.path.join("reference_data", "extinction")
+    for f in pkg_resources.resource_listdir("specreduce-data", ext_dir):
+        if ".dat" in f:
+            ext_path = pkg_resources.resource_filename(
+                "specreduce-data",
+                os.path.join(ext_dir, f)
+            )
+            t = read_basic_ascii_table(ext_path)
+            assert(len(t) > 0)
+            assert(len(t.columns) == 2)
